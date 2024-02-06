d2736161aebc0a98c4e0e87396f5a628c8b256d6
Junsik Shim <junsik.shim@gmail.com>
2024-02-06 23:01:12 +0900

    Change logo image.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,7 @@
 import Link from 'next/link';
+import Image from 'next/image';
+
+import Logo from '@/public/logo.svg';
 
 interface NavbarProps {
   selectedItem: string | null;
@@ -7,7 +10,9 @@ interface NavbarProps {
 const Navbar = ({ selectedItem }: NavbarProps) => {
   return (
     <header className="h-14 flex flex-row items-baseline justify-between px-3 py-2 box-border bg-[#EF233C] text-white">
-      <h1 className="text-3xl font-bold">Workfloor</h1>
+      <h1 className="text-3xl font-bold">
+        <Image src={Logo} alt="Logo" height="32" className="mt-1.5 ml-2.5" />
+      </h1>
 
       <nav>
         <ul className="flex flex-row gap-4">
